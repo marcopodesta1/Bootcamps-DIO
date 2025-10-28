7a92fab70240ccff95c3974d6005e0ce43f5c537
marcopodesta1 <marcopodesta@hotmail.com>
2025-10-28 00:21:23 -0300

    commit DesafioPOO

---
+++ C#/Avanade-Back-end-com-.NET-e-IA/Desafios/DesafioPOO/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("123456789", "iPhone 13", "IMEI123456789", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("987654321", "Nokia 7.2", "IMEI987654321", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
